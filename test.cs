dfcd3b9d124415b39358b9e06a35011a4f3c0457
krivonosovna <ymerollin@gmail.com>
2024-06-13 14:43:51 +0300

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,3 +1,43 @@
-﻿int a = 5;
-int b = 3;
-Console.WriteLine(a + b);
+﻿while (true)
+{
+    Random random = new();
+    int a = random.Next(1, 4);
+    int b = random.Next(1, 4);
+    if (a == b)
+    {
+        continue;
+    }
+    else
+    {
+        if (a + b == 3)
+        {
+            Console.WriteLine("Диллер выложил на стол табак и бумагу");
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine("Курильщик со спичками куриt..."+i);
+                Thread.Sleep(1000);
+            }
+        }
+        else
+        {
+            if (a + b == 4)
+            {
+                Console.WriteLine("Диллер выложил на стол табак и спички");
+                for (int i = 10; i >= 0; i--)
+                {
+                    Console.WriteLine("Курильщик с бумагой курит..." + i);
+                    Thread.Sleep(1000);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Диллер выложил на стол спички и бумагу");
+                for (int i = 10; i >= 0; i--)
+                {
+                    Console.WriteLine("Курильщик с табаком курит..." + i);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
